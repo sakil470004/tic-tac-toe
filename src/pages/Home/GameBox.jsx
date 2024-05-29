3a3f272bd35802a79ced5ca1333019c2159e052e
Mynul Islam <mynul.sakil@gmail.com>
2024-05-29 20:23:19 +0600

    setting game logic

--- src/pages/Home/GameBox.jsx
+++ src/pages/Home/GameBox.jsx
@@ -1,10 +1,11 @@
 import xImg from "../../assets/x.png";
 import oImg from "../../assets/o.png";
-function GameBox({ index, value, gameLogic, setGameLogic, player, setPlayer }) {
+function GameBox({ index, value, gameLogic, setGameLogic, player, setPlayer,winningLogic }) {
   return (
     <div className=" w-48 h-48 border-4 bg-blue-500 border-blue-500">
       <div className="w-full p-2 h-full bg-white"
       onClick={() => {
+        // if the box is empty then only we can change the value
         if (gameLogic[index] === 0) {
           setGameLogic((prev) => {
             const newGameLogic = [...prev];
@@ -17,7 +18,7 @@ function GameBox({ index, value, gameLogic, setGameLogic, player, setPlayer }) {
       >
         {gameLogic[index] ? (
           <img
-            src={player ? xImg : oImg}
+            src={gameLogic[index]===1 ? xImg : oImg}
             alt="tic tac toe piece"
             className="w-full h-full object-cover"
           />
--- src/pages/Home/GameUi.jsx
+++ src/pages/Home/GameUi.jsx
@@ -6,6 +6,16 @@ export default function GameUi() {
   //   here player 1 is represented by 1 and player 2 is represented by 2
   //  player 1 is represented by x and player 2 is represented by o
   // player true means player 1 and false means player 2
+  const winningLogic = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
   const [player, setPlayer] = useState(true);
   return (
     <div className="grid grid-cols-3 ">
@@ -19,6 +29,7 @@ export default function GameUi() {
             setGameLogic={setGameLogic}
             player={player}
             setPlayer={setPlayer}
+            winningLogic={winningLogic}
           />
         );
       })}
